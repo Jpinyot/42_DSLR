72256f7a79f9ea267158c14a9d80a2b7696c9890
jpinyot <jpinyot@student.42.fr>
2020-02-25 10:30:32 +0100

    WIP, creating a std csv reader

---
+++ includes/csvReader.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   csvReader.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpinyot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/25 07:41:28 by jpinyot           #+#    #+#             */
+/*   Updated: 2020/02/25 10:28:52 by jpinyot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "csvReader.h"
+
+void	CsvData::getData()
+{
+	ifstream file(fileDescriptor_);
+	if (!file || !getline(file, subsets_)){
+		string strError;
+		strError.append("Espected file ");
+		strError.append(fileDescriptor_);
+		strError.append(" not found.");
+		throw runtime_error(strError);
+	}
+	subsets_.indexing();
+	/* cout << subsets_ << "\n\n"; */
+	string line = "";
+	while (getline(file,line)){
+		lines_.emplace_back(line);
+		cout << lines_.back();
+	}
+	
+}
+
+int	main()
+{
+	CsvData data("files/dataset_train.csv");
+
+	return (0);
+}
+
+/* int main() { */
+/*     for (int i=0; i<1800; i++) { */
+/*         for (int j=0; j<1800; j++) */
+/*             std::cout<<rand()/double(RAND_MAX)<<","; */
+/*         std::cout << "\n"; */
+/*     } */
+/* } */
---
+++ includes/csvReader.h
@@ -0,0 +1,142 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   csvReader.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpinyot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/25 07:39:51 by jpinyot           #+#    #+#             */
+/*   Updated: 2020/02/25 10:27:04 by jpinyot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+/* #include <time.h> */
+/* #include <stdlib.h> */
+/* #include <locale> */
+/* #include <sstream> */
+/* #include <algorithm> */
+/* #include <iterator> */
+
+#define DELIMETER ','
+
+using namespace std;
+
+	/* line trasnformet to a vector of double */
+/* class CsvVector : public vector<double> */
+/* { */
+	/* private: */
+	/* 	string			line_; */
+
+	/* public: */
+	/* 	CsvVector(): */
+	/* 		line_(0) */
+	/* 	{}; */
+/* }; */
+
+	/* line from the csv file */
+class CsvLine : public string
+{
+	private:
+
+	public:
+		CsvLine()
+		{};
+		virtual ~CsvLine() {};
+};
+
+	/* the data subsets, data descriptors */
+class CsvSubsets : public CsvLine
+{
+	private:
+		vector<int>		deprecates_;
+		vector<int>		subsetsIndex_;
+		int				size_;
+
+	public:
+		CsvSubsets():
+			deprecates_(0), subsetsIndex_(0), size_(1)
+		{};
+		virtual ~CsvSubsets() {};
+
+		void	indexing()
+		{
+			subsetsIndex_.emplace_back(0);
+			int	nextDelimeter = 0;
+			while ((nextDelimeter = this->find(DELIMETER, nextDelimeter + 1)) != -1){
+				subsetsIndex_.emplace_back(nextDelimeter);
+				size_ += 1;
+			}
+		}
+};
+
+	/* main class */
+class CsvData
+{
+	private:
+		string					fileDescriptor_;
+		vector<CsvLine>			lines_;
+		vector<vector<double> >	data_;
+		CsvSubsets				subsets_;
+		/* vector<CsvVector>	data_; */
+
+		void	getData();
+
+	public:
+	CsvData(string fileDescriptor):
+		fileDescriptor_(fileDescriptor), lines_(0), data_(0), subsets_()
+	{
+		getData();
+	};
+	virtual	~CsvData() {};
+};
+
+/* class my_ctype : public std::ctype<char> { */
+/* 	public: */
+/* 	    std::vector<mask> my_table; */
+/* 	public: */
+/* 		my_ctype(size_t refs=0): */
+/* 			my_table(table_size), std::ctype<char>(my_table.data(), false, refs) */ 
+/* 		{ */
+/* 			std::copy_n(classic_table(), table_size, my_table.data()); */
+/* 			my_table[',']=(mask)space; */
+/* 		} */
+/* }; */
+
+/* template <class T> */
+/* class converter { */
+/* 	public: */
+/* 		std::stringstream	buffer; */
+/* 		my_ctype			*m; */
+/* 		std::locale			l; */
+/* 	public: */
+/* 	    converter(): */
+/* 			m(new my_ctype), l(std::locale::classic(), m) */
+/* 		{ */
+/* 			buffer.imbue(l); */
+/* 		} */
+
+/*     	std::vector<T> operator()(std::string const &in) */
+/* 		{ */
+/*         	buffer.clear(); */
+/*         	buffer<<in; */
+/*         	return std::vector<T> {std::istream_iterator<T>(buffer),std::istream_iterator<T>()}; */        
+/*     	} */
+/* }; */
+
+/* int main() { */
+/*     std::ifstream						in("somefile.csv"); */
+/*     std::vector<std::vector<double>>	numbers; */
+
+/*     std::string			line; */
+/*     converter<double>	cvt; */
+
+/*     clock_t start=clock(); */
+/*     while (std::getline(in, line)) */
+/*         numbers.push_back(cvt(line)); */
+/*     clock_t stop=clock(); */
+/*     std::cout<<double(stop-start)/CLOCKS_PER_SEC << " seconds\n"; */
+/* } */
